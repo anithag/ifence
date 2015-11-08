606f4a6022fce082dc0704fc97551c3940d975be
Anitha Gollamudi <agollamudi@g.harvard.edu>
2015-11-08 09:28:39 -0500

    Translation Outputs

--- testsuite/run.sh
+++ testsuite/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rm -f *.log *.opb *.out output.txt 
+rm -f output/*.log output/*.opb *.out output/output.txt 
 for i in *.src
 do
 	base=`basename $i .src`
-	../autopar $i >& $base.log
-	cp min.opb $base.opb
-	cp output.txt $base.out
+	../autopar $i >& output/$base.log
+	cp min.opb output/$base.opb
+	cp output.txt output/$base.out
 done
